a8d2c516f59919a5bde7fb2f3d735d20c6588060
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-24 12:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let form = document.querySelector("form")
+let age = document.getElementById("age")
+let name = document.getElementById("name")
+
+// validation
+form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (age.value === "" || name.value === "") {
+          alert("Please enter valid details");
+        }
+        if (age.value > 18) {
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(`Welcome, ${name.value}. You can vote.`);
+            }, 1000);
+          })
+            .then((msg) => {
+              alert(msg);
+            })
+            .catch((msg) => {
+              console.log(msg);
+            });
+        }else{
+            new Promise((_,reject)=>{
+                setTimeout(()=>{
+                    reject(`Oh sorry ${name.value}. You aren't old enough.`)
+                },4000)
+            })
+            .catch((msg)=>{
+                alert(msg)
+            })
+        }
+      });
